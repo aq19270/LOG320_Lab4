61ada927084f2332eacac2e7865836f6983b1c7d
Jimmy Tremblay-Bernier 01 <jimmy.tremblay-bernier.1@ens.etsmtl.ca>
2022-11-16 02:00:51 -0500

    partially implemented Minmax

--- src/main/java/Laboratoire4/Minmax.java
+++ src/main/java/Laboratoire4/Minmax.java
@@ -1,34 +1,54 @@
 package Laboratoire4;
 
+import Laboratoire4.Movement;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Minmax {
-    public static double minmax(int[][] board, int depth, Boolean isMaximizing) {
+    public static int minmax(int[][] board, int depth, Boolean isMaximizing, int playerColor) {
         // if (gameOver) {
         // return +/- infini;
         // }
         if (depth == 0) {
-            return 0;
-            // return evaluation(board)
+            return Movement.evaluateBoard(board, playerColor);
         }
         if (isMaximizing) {
-            double bestScore = Double.MIN_VALUE;
+            int bestScore = Integer.MIN_VALUE;
             // String[] possibleMove = availableMove(board);
+            ArrayList<String> possibleMoves = Movement.generateAllPossibleMoves(board, playerColor);
+            Iterator<String> iterator = possibleMoves.iterator();
+
             // for (int i=0;i<possibleMove.length;i++){
-            // Faire le move i sur le board
-            // score = minmax(board,depth-1,false)
-            // Undo le move i sur le board
-            // bestScore = max(score,bestScore)
-            // }
-            return bestScore;
-        } else {
-            double bestScore = Double.MAX_VALUE;
-            // String[] possibleMove = availableMove(board);
-            // for (int i=0;i<possibleMove.length;i++){
-            // Faire le move i sur le board
-            // score = minmax(board,depth-1,true)
-            // Undo le move i sur le board
-            // bestScore = min(score,bestScore)
-            // }
+            while (iterator.hasNext()) {
+                //TODO Faire le move i sur le board
+
+                // score = minmax(board,depth-1,false)
+                int score = minmax(board,depth-1,false, playerColor);
+
+                //TODO Undo le move i sur le board
+
+                bestScore = Math.max(bestScore, score);
+            }
             return bestScore;
         }
+
+        int worstScore = Integer.MAX_VALUE;
+        // String[] possibleMove = availableMove(board);
+        ArrayList<String> possibleMoves = Movement.generateAllPossibleMoves(board, playerColor);
+        Iterator<String> iterator = possibleMoves.iterator();
+
+        // for (int i=0;i<possibleMove.length;i++){
+        while (iterator.hasNext()) {
+            //TODO Faire le move i sur le board
+
+            // score = minmax(board,depth-1,true)
+            int score = minmax(board,depth-1,false, playerColor);
+
+            //TODO Undo le move i sur le board
+
+            worstScore = Math.min(worstScore, score);
+        }
+        return worstScore;
     }
 }

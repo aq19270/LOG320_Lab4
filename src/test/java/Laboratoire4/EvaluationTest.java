ee072a4e86b9178579997af71161917d2d436ce7
etsilhamenouri <ilhame.nouri.1@ens.etsmtl.ca>
2022-12-07 15:17:27 -0500

    test capture

--- src/test/java/Laboratoire4/EvaluationTest.java
+++ src/test/java/Laboratoire4/EvaluationTest.java
@@ -259,4 +259,63 @@ public class EvaluationTest {
             }
         }
     }
+
+    @Test
+    public void captureTest() {
+        board = new Board(8, 8);
+        board.setPlayerColor(Pion.colors.white);
+        board.setEnnemyColor(Pion.colors.black);
+
+        int [][] tempBoard = {
+                { // X1
+                        0,0,0,4,0,0,0,0
+                },
+                { //X2
+                        2,0,0,4,0,0,0,2
+                },
+                { //X3
+                        2,0,0,4,4,0,0,2
+                },
+                { //X4
+                        2,0,0,4,4,0,0,2
+                },
+                { //X5
+                        2,0,0,4,4,0,0,2
+                },
+                { //X6
+                        0,0,0,2,0,0,0,2
+                },
+                {  //X7
+                        2,0,0,0,0,0,0,2
+                },
+                { //X8
+                        0,0,0,0,0,0,0,0
+                }
+        };
+
+        board.getPionsBlanc().clear();
+        board.getPionsNoir().clear();
+
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                if(tempBoard[x][y] == 0) {
+                    board.getCase(x, y).emptyCase();
+                    continue;
+                }
+
+                if(tempBoard[x][y] == 4) {
+                    Pion pion = new Pion(x, y, Pion.colors.white);
+                    board.addPionBlanc(pion);
+                    board.getCase(x, y).setPion(pion);
+                    continue;
+                }
+
+                Pion pion = new Pion(x, y, Pion.colors.black);
+                board.addPionNoir(pion);
+                board.getCase(x, y).setPion(pion);
+            }
+        }
+
+        Assert.assertNotEquals("", Minmax.findBestMove(board, 3, true), 0.000000001);
+    }
 }
